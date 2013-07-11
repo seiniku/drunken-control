d31ed01b2b5c710f3602dbcba103486f70dd877a
seiniku <justin@keppers.org>
2013-07-11 18:58:41 +0000

    initial commit of threaded temp control

---
+++ drunken-control.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+import time
+from kettle import Kettle
+from yaml import load, dump
+import threading
+try:
+    from yaml import CLoader as Loader, CDumper as Dumper
+except ImportError:
+    from yaml import Loader, Dumper
+
+try:
+    f = open('config.yaml')
+    data = load(f, Loader=Loader)
+    f.close()
+except IOError:
+    print "Error: Cannot find config file"
+    exit(1)
+
+#The list of kettles defined in the config file
+kettles = {}
+for config in data:
+    kettles[config] =  Kettle(conf=data[config])
+    kettles[config].start()
+#Reads the config file  every five second and checks for changes. If changes are found it sets enabled and target for each kettle defined
+while True:
+    f = open('config.yaml','r')
+    newdata = load(f, Loader=Loader)
+    f.close()
+    if data != newdata:
+        print "reloading config"
+        print data
+        print newdata
+        for config in newdata:
+           kettles[config].setEnabled(newdata[config]["enabled"])
+           kettles[config].setTarget(newdata[config]["target"])
+    data = newdata
+    time.sleep(5)
