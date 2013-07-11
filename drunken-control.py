a3002eaac4e973844a55f68ddfb034d20b9d9df0
seiniku <justin@keppers.org>
2013-07-11 20:27:58 +0000

    added __main__

--- drunken-control.py
+++ drunken-control.py
@@ -7,31 +7,33 @@ try:
     from yaml import CLoader as Loader, CDumper as Dumper
 except ImportError:
     from yaml import Loader, Dumper
-
-try:
-    f = open('config.yaml')
-    data = load(f, Loader=Loader)
-    f.close()
-except IOError:
-    print "Error: Cannot find config file"
-    exit(1)
+def start():
+    try:
+        f = open('config.yaml')
+        data = load(f, Loader=Loader)
+        f.close()
+    except IOError:
+        print "Error: Cannot find config file"
+        exit(1)
 
 #The list of kettles defined in the config file
-kettles = {}
-for config in data:
-    kettles[config] = Kettle(conf=data[config], name=config)
-    kettles[config].start()
+    kettles = {}
+    for config in data:
+        kettles[config] = Kettle(conf=data[config], name=config)
+        kettles[config].start()
 #Reads the config file  every five second and checks for changes. If changes are found it sets enabled and target for each kettle defined
-while True:
-    f = open('config.yaml','r')
-    newdata = load(f, Loader=Loader)
-    f.close()
-    if data != newdata:
-        print "reloading config"
-        print data
-        print newdata
-        for config in newdata:
-           kettles[config].setEnabled(newdata[config]["enabled"])
-           kettles[config].setTarget(newdata[config]["target"])
-    data = newdata
-    time.sleep(5)
+    while True:
+        f = open('config.yaml','r')
+        newdata = load(f, Loader=Loader)
+        f.close()
+        if data != newdata:
+            print "reloading config"
+            print data
+            print newdata
+            for config in newdata:
+               kettles[config].setEnabled(newdata[config]["enabled"])
+               kettles[config].setTarget(newdata[config]["target"])
+        data = newdata
+        time.sleep(5)
+if __name__ == '__main__':
+    start()
